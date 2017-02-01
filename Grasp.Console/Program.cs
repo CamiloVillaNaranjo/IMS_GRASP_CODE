e1bd87db39a2f3622fd2244888ec58303eab931a
cvillanaranjo <camilo.villa@yuxiglobal.com>
2017-01-31 19:11:07 -0500

    Rearranging code

---
+++ Grasp.Console/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Grasp.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- Grasp.Library/Grasp.Library/Class1.cs
+++ Grasp.Library/Grasp.Library/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Grasp.Library
+﻿namespace Grasp.Library
 {
     public class Class1
     {
